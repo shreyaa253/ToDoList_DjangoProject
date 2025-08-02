61b656995351c8d501204a26f57c0c2d3e89ef8c
Shreya K <shreyakalamkar17@gmail.com>
2025-08-02 21:03:09 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    date = models.DateTimeField()
+    def __str__(self):
+        return self.title
